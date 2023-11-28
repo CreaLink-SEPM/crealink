034c7e12b20ad653ac1c9d5abbc17b507f1a5947
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2023-11-28 19:49:40 +0700

    Modify .env and db config

--- backend/components/configs/db.js
+++ backend/components/configs/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.DB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
