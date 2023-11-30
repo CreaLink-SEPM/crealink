75429107eb03e615faead82e19380cbe04f4d089
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2023-11-30 09:57:50 +0700

    Create upload file feature using multer

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const routes = require("./components/routes/router.js");
+const multer = require("multer");
 
 dotenv.config();
 
@@ -12,6 +13,23 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './components/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname) 
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
--- backend/components/controllers/postController.js
+++ backend/components/controllers/postController.js
@@ -1,6 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 const Post = require('../models/postModel');
-exports.createPost = async (req, res, next) => {
+const {validationResult} = require('express-validator');
 
+exports.createPost = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const error = new Error('Validation failed, entered data is not correct');
+            error.statusCode = 422;
+            throw error;
+        }
+        const title = req.body.title;
+        const content = req.body.content;
+        const imageUrl = req.file.path;
+
+    }
+    catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500
+        }
+        next(err);
+    }
 }
