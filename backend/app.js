bc11a8f7d8070fc3f5493e662449e9767c9309d2
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2023-11-30 11:40:37 +0700

    Fixing app.js

--- backend/app.js
+++ backend/app.js
@@ -12,14 +12,12 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 const app = express();
-
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
-
-
+routes(app);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
