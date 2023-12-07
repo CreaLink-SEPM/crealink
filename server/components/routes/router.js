c2f90151a3fca62c760b77b929d176136a820fc2
Khangdepzai <khangtgr@gmail.com>
2023-12-07 14:03:14 +0700

    Intergrate Google Authenticator alone successfully

--- server/components/routes/router.js
+++ server/components/routes/router.js
@@ -1,9 +1,11 @@
 const UserRouter = require("./userRoutes.js");
 const PostRouter = require("./postRoutes.js");
+const GoogleAuthRouter = require("./googleAuthRoutes.js");
 
 const routes = (app) => {
   app.use("/api/user", UserRouter);
   app.use("/api/feed", PostRouter);
+  app.use("/api/auth", GoogleAuthRouter);
 };
 
 module.exports = routes;
--- server/components/routes/userRoutes.js
+++ server/components/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const UserController = require("../controllers/userController.js");
 const AuthMiddleware = require("../middlewares/authMiddleware.js");
 
+// User routes
 router.post("/register", UserController.registerUser);
 router.post("/login", UserController.loginUser);
 router.post("/logout", UserController.logoutUser);
