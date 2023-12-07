5193eb6be6df12cb6cc5ce4e5258ee22bde56e8c
Khangdepzai <khangtgr@gmail.com>
2023-12-07 15:29:15 +0700

    Add .env sample file & Update awsS3Util.js

--- server/components/utils/awsS3Util.js
+++ server/components/utils/awsS3Util.js
@@ -1,22 +1,25 @@
 const AWS = require("aws-sdk");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // Set your AWS credentials
 const credentials = new AWS.Credentials({
-  accessKeyId: "YOUR_ACCESS_KEY_ID",
-  secretAccessKey: "YOUR_SECRET_ACCESS_KEY",
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
 
 // Configure the AWS SDK
 AWS.config.credentials = credentials;
-AWS.config.update({ region: "YOUR_AWS_REGION" }); // Replace with your AWS region
+AWS.config.update({ region: "ap-southeast-1" });
 
 // Create an S3 instance
 const s3 = new AWS.S3();
 
 // Function to upload an object to S3 bucket
-function uploadToS3(bucketName, fileName, fileData) {
+function uploadToS3(fileName, fileData) {
   const params = {
-    Bucket: bucketName,
+    Bucket: "crealink-images",
     Key: fileName,
     Body: fileData,
   };
@@ -33,9 +36,9 @@ function uploadToS3(bucketName, fileName, fileData) {
 }
 
 // Function to delete an object from S3 bucket
-function deleteFromS3(bucketName, fileName) {
+function deleteFromS3(fileName) {
   const params = {
-    Bucket: bucketName,
+    Bucket: "crealink-images",
     Key: fileName,
   };
 
