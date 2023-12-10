dca449d00bf97323cb8b10d71c55348c92d4deb6
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2023-12-10 22:58:50 +0700

    Resolve conflict

--- client/lib/AntRegistry.tsx
+++ client/lib/AntRegistry.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import React from 'react';
-// import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
-// import type Entity from '@ant-design/cssinjs/es/Cache';
-// import { useServerInsertedHTML } from 'next/navigation';
+import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
+import type Entity from '@ant-design/cssinjs/es/Cache';
+import { useServerInsertedHTML } from 'next/navigation';
 
 const StyledComponentsRegistry = ({ children }: React.PropsWithChildren) => {
   const cache = React.useMemo<Entity>(() => createCache(), []);
