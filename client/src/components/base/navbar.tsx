8d8fe079b583521a62369f3a930edcb28ab13f69
JustAbusiness <huysanti123456@gmail.com>
2023-12-12 11:39:58 +0700

    fix path error

--- client/src/components/base/navbar.tsx
+++ client/src/components/base/navbar.tsx
@@ -1,15 +1,15 @@
 'use client';
 import React, { useState } from 'react';
 import { Tabs } from 'antd';
-import homeIcon from '../../../public/assets/icons/Home Icon.svg';
-import searchIcon from '../../../public/assets/icons/Search Icon.svg';
-import heartIcon from '../../../public/assets/icons/Heart Icon.svg';
-import messageIcon from '@../../../public/assets/icons/Message Icon.svg';
-import userIcon from '../../../public/assets/icons/User Icon.svg';
+import homeIcon from '@/public/assets/icons/Home Icon.svg';
+import searchIcon from '@/public/assets/icons/Search Icon.svg';
+import heartIcon from '@/public/assets/icons/Heart Icon.svg';
+import messageIcon from '@/public/assets/icons/Message Icon.svg';
+import userIcon from '@/public/assets/icons/User Icon.svg';
 import SearchBar from '../explore/SearchBar';
 import Image from 'next/image';
-import Loading from '../common/Loading';
 import ProfilePage from '../common/ProfilePage';
+import Loading from '../common/Loading';
 
 
 function Navbar() {
