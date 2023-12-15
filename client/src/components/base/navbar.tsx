175e972ac839fd64997db6cba327e20d1b9ad1cc
JustAbusiness <huysanti123456@gmail.com>
2023-12-16 01:07:20 +0700

    fix files in order

--- client/src/components/base/navbar.tsx
+++ client/src/components/base/navbar.tsx
@@ -5,26 +5,24 @@ import searchIcon from '@/public/assets/icons/Search Icon.svg';
 import heartIcon from '@/public/assets/icons/Heart Icon.svg';
 import messageIcon from '@/public/assets/icons/Message Icon.svg';
 import userIcon from '@/public/assets/icons/User Icon.svg';
-import SearchBar from '../explore/SearchBar';
-import Link from "next/link";
+import Link from 'next/link';
 import Image from 'next/image';
 
-
 function Navbar() {
-    const icons = [homeIcon, searchIcon, heartIcon, messageIcon, userIcon];
-    const hrefs = ['/', '/search', '/favorites', '/notifications', '/profile'];
+  const icons = [homeIcon, searchIcon, heartIcon, messageIcon, userIcon];
+  const hrefs = ['/', '/search', '/favorites', '/notifications', '/profile'];
 
-    return (
-        <div className='w-[600px] ml-[8%] flex justify-between'>
-            {icons.map((icon, index) => (
-                <Link key={index} href={hrefs[index]} passHref>
-                    <div className='flex flex-col items-center cursor-pointer'>
-                        <Image src={icon} alt={`Icon ${index + 1}`} width={30} height={30} />
-                    </div>
-                </Link>
-            ))}
-        </div>
-    );
+  return (
+    <div className="w-[600px] ml-[8%] flex justify-between">
+      {icons.map((icon, index) => (
+        <Link key={index} href={hrefs[index]} passHref>
+          <div className="flex flex-col items-center cursor-pointer">
+            <Image src={icon} alt={`Icon ${index + 1}`} width={30} height={30} />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Navbar;
