beadf5e85dbbbd81ad573228dd1194f1ec44a4a7
JustAbusiness <huysanti123456@gmail.com>
2023-12-16 16:08:50 +0700

    create notificatons page

--- client/src/app/error.tsx
+++ client/src/app/error.tsx
@@ -1,12 +1,14 @@
-import { Button } from 'antd';
-import Link from 'next/link';
-import Navbar from '../components/base/navbar';
+'use client'
 import Image from 'next/image';
-import { Suspense } from 'react';
-import AvatarSetting from '../components/common/AvatarSetting';
+import Link from 'next/link';
+import React, { Suspense } from 'react';
 import Loading from '../components/common/loading';
+import Navbar from '../components/base/navbar';
+import AvatarSetting from '../components/common/AvatarSetting';
+import { Button } from 'antd';
+import './globals.css';
 
-export default function NotFound() {
+function Error() {
   return (
     <div>
       <div className="flex flex-col w-full xl:container">
@@ -30,12 +32,12 @@ export default function NotFound() {
       </div>
 
       <div className="grid place-items-center m-[200px]">
-        <div className="">
+        <div className="text-center">
           <p className="font-bold text-[#1a2e35] text-4xl leading-1 mt-2">Ooops....</p>
-          <p className="font-bold text-4xl text-[#1a2e35] leading-1 mt-3">404 Error</p>
-          <div className="text-4xl text-[#1a2e35] mt-3"> Page not found</div>
+          <p className="font-bold text-4xl text-[#1a2e35] leading-1 mt-3">500 Error</p>
+          <div className="text-4xl text-[#1a2e35] mt-3 text-center"> Server not found</div>
         </div>
-        <Button className="w-[250px] border-none outline-none h-[60px] hover:bg-[#a2383a] rounded-[15px] bg-[#a20103] text-white text-2xl font-semibold mt-10">
+        <div className="w-[250px] h-[60px] p-4 hover:bg-[#a2383a] rounded-[15px] bg-[#a20103] text-white text-2xl font-semibold mt-10">
           <Link href="/" className="flex items-center justify-center gap-10">
             Go Back
             <svg
@@ -49,8 +51,10 @@ export default function NotFound() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
             </svg>
           </Link>
-        </Button>
+        </div>
       </div>
     </div>
   );
 }
+
+export default Error;
