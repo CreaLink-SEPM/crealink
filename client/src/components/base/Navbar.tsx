b82db3775ad18a7fd74abb3a6f5502311a3fb883
james-dinhh <jamesdinh.bib@gmail.com>
2023-12-17 23:32:44 +0700

    resolve conflicts again :>

--- client/src/components/base/Navbar.tsx
+++ client/src/components/base/Navbar.tsx
@@ -3,17 +3,16 @@ import React from 'react';
 import homeIcon from '@/public/assets/icons/Home Icon.svg';
 import searchIcon from '@/public/assets/icons/Search Icon.svg';
 import heartIcon from '@/public/assets/icons/Heart Icon.svg';
-import messageIcon from '@/public/assets/icons/Message Icon.svg';
+// import messageIcon from '@/public/assets/icons/Message Icon.svg';
 import notificationIcon from '@/public/assets/icons/Notification Icon.svg';
 import userIcon from '@/public/assets/icons/User Icon.svg';
 import Link from 'next/link';
 import Image from 'next/image';
 
 function Navbar() {
-  const icons = [homeIcon, searchIcon, heartIcon, messageIcon, userIcon];
-  const hrefs = ['/home', '/search', '/favorites', '/notifications', '/profile'];
   const icons = [homeIcon, searchIcon, heartIcon, notificationIcon, userIcon];
-  const hrefs = ['/', '/search', '/favorites', '/notifications', '/profile'];
+  const hrefs = ['/home', '/search', '/favorites', '/notifications', '/profile'];
+
 
   return (
     <div className="w-[600px] ml-[8%] flex justify-center space-x-20">
