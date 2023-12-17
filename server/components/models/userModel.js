7c18b22e740fd07f62783a4c7f5382d5c7fa7734
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2023-12-17 23:29:46 +0700

    Update user model

--- server/components/models/userModel.js
+++ server/components/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true, index: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, deault: false},
+    isAdmin: { type: Boolean, default: false},
     posts: [{ type: Schema.Types.ObjectId, ref: "Post" }]
   },
   {
