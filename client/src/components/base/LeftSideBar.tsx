5eef2f45ac134e5eb2b57686179daa39e5b3a2f5
JustAbusiness <huysanti123456@gmail.com>
2023-12-23 06:02:59 +0700

    first push

--- client/src/components/base/LeftSideBar.tsx
+++ client/src/components/base/LeftSideBar.tsx
@@ -1,23 +1,38 @@
 'use client';
-import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 function LeftSideBar() {
   return (
-    <div className="w-[20%] text-center bottom-0">
+    <Link href="/help-center" className="w-[20%] text-center bottom-0 h-auto">
       <div className="w-35 h-14 justify-center items-center inline-flex">
         <div className="self-stretch justify-center items-center inline-flex">
-          <div className="absolute bottom-[50%] self-stretch px-6 py-5 bg-red-800 rounded-full shadow border border-black border-opacity-5 justify-center items-start gap-px inline-flex">
+          <div className="absolute bottom-[50%] self-stretch pl-3 pr-3 py-5 bg-red-800 rounded-full shadow border border-black border-opacity-5 justify-center items-start inline-flex">
             <div className="self-stretch py-px justify-center items-center inline-flex">
-              <div className="self-stretch justify-center items-center inline-flex">
-                <div className="w-14 h-4 text-white text-base font-semibold font-['Roboto'] leading-tight">For you</div>
-                <Image src="/assets/icons/switch2side.png" className='ml-2' alt="setting" width={15} height={20} />
+              <div className="justify-center items-center inline-flex gap-4">
+                <div className="w-20 h-4 text-white text-center text-base font-semibold font-['Roboto'] leading-tight">
+                  <span className="w-[100px] block"> Help Center </span>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="white"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                  />
+                </svg>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
