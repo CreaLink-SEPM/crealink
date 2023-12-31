937ce7fd4ace54b130b42024c458438e8f37b047
JustAbusiness <huysanti123456@gmail.com>
2023-12-31 09:59:43 +0700

    set default page to /home

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+const nextConfig = {
+    basePath,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: `${basePath}/home`,
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- client/src/app/(front)/page.tsx
+++ client/src/app/(front)/page.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import '../globals.css';
 
 
 export default async function Home() {
   return (
     <div className="flex flex-col w-full xl:container">
-    
+         
     </div>
   );
 }
