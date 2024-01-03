96e8feeac3a0db2936a6ecddc2c5c31a8e629ba1
Khangdepzai <khangtgr@gmail.com>
2024-01-03 11:04:48 +0700

    Remove testing components

--- server/source/com/configs/openai.js
+++
@@ -1,9 +0,0 @@
-const {OpenAI} = require("openai");
-const dotenv = require("dotenv");
-dotenv.config();
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-  });
-module.exports = {
-    openai
-}
