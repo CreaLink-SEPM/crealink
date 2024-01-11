90e8c1398d3457bf7328d2cd42e297693f89c498
Khangdepzai <khangtgr@gmail.com>
2024-01-11 10:34:06 +0700

    New changes added

--- server/source/components/models/userModel.js
+++ server/source/components/models/userModel.js
@@ -7,26 +7,50 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false},
-    user_image: {type: String, default: 'https://crealink-images.s3.ap-southeast-1.amazonaws.com/avatar/istockphoto-1451587807-612x612.jpg'},
+    isAdmin: { type: Boolean, default: false },
+    user_image: {
+      type: String,
+      default:
+        "https://crealink-images.s3.ap-southeast-1.amazonaws.com/avatar/istockphoto-1451587807-612x612.jpg",
+    },
     posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
-    following: [{ 
-      id: { type: Schema.Types.ObjectId, ref: "User" },
-      name: { type: String, required: true },
-      username: { type: String },
-      user_image: {type: String, default: 'https://crealink-images.s3.ap-southeast-1.amazonaws.com/avatar/istockphoto-1451587807-612x612.jpg'},
-    }],
-    followers: [{
-      id: { type: Schema.Types.ObjectId, ref: "User" },
-      name: { type: String, required: true },
-      username: { type: String },
-      user_image: {type: String, default: 'https://crealink-images.s3.ap-southeast-1.amazonaws.com/avatar/istockphoto-1451587807-612x612.jpg'},
-    }],    image: { type: String },
+    following: [
+      {
+        id: { type: Schema.Types.ObjectId, ref: "User" },
+        name: { type: String, required: true },
+        username: { type: String },
+        user_image: {
+          type: String,
+          default:
+            "https://crealink-images.s3.ap-southeast-1.amazonaws.com/avatar/istockphoto-1451587807-612x612.jpg",
+        },
+      },
+    ],
+    followers: [
+      {
+        id: { type: Schema.Types.ObjectId, ref: "User" },
+        name: { type: String, required: true },
+        username: { type: String },
+        user_image: {
+          type: String,
+          default:
+            "https://crealink-images.s3.ap-southeast-1.amazonaws.com/avatar/istockphoto-1451587807-612x612.jpg",
+        },
+      },
+    ],
+    image: { type: String },
     bio: { type: String, default: "" },
     is_verified: { type: Boolean, default: false },
     isAdmin: { type: Boolean, default: false },
     posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
-    savedPosts: [{ type: Schema.Types.ObjectId, ref: "Post" }]
+    notifications: [
+      {
+        content: { type: String, required: true },
+        timestamp: { type: Date, default: Date.now },
+        read: { type: Boolean, default: false },
+      },
+    ],
+    savedPosts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   },
   {
     timestamps: true,
