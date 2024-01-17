e7f9f28c04de0f49e77b7d0ede74a91c286e19c8
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-01-17 12:49:37 +0700

    Update comment

--- client/src/components/common/Comment.tsx
+++ client/src/components/common/Comment.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import type Icon from '@ant-design/icons';
-import { LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
-import { Avatar, List, Skeleton, Switch } from 'antd';
+import { Avatar, List, Skeleton, Form, Button, Input, Modal,  Typography, Comment as AntComment } from 'antd';
+import { useSession } from 'next-auth/react';
+
 
 interface Comment {
   _id: string;
@@ -16,62 +17,110 @@ interface Comment {
 }
 
 interface CommentProps {
-  postId: string;
-  token: string;
+  postId: string,
+  token: string,
+  modal20pen: boolean,
 }
 
-
-const Comment: React.FC<CommentProps> = ({ postId, token }) => {
+const Comment: React.FC<CommentProps> = ({ postId, token, modal20pen }) => {
   const [comments, setComments] = useState<Comment[]>([]);
+  console.log('COMMENTS ', comments);
   const [newComment, setNewComment] = useState<string>('');
+  const {data: session} = useSession(); 
+  const [visible, setVisible] = useState<boolean>(false);
+  const [loadingInitial, setLoadingInitial] = useState<boolean>(true);
+  const [openModal, setOpenModal] = useState<boolean>(false)
+  const [isModalOpen, setIsModalOpen] = useState();
 
-  useEffect(() => {
-    // Fetch comments when the component mounts
-    fetchComments();
-  }, [postId]);
+  const showModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const fetchComments = async () => {
     try {
-      const response = await axios.get(`http://localhost:[port]/api/comment/${postId}`, {
+      const response = await axios.get(`http://54.169.199.32:5000/api/comment/${postId}`, {}, {
         headers: {
-          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
       });
-
-      setComments(response.data.comments);
+      const data = response.data.comments;
+      console.log('RESPONSE COMMENTS', data);
+      setComments(data);
     } catch (error) {
-      console.error('Error fetching comments:', error.message);
+      console.error('Error fetching comments:', error);
+    } finally {
+      setLoadingInitial(false);
     }
   };
 
-  const handleCreateComment = async () => {
-    try {
-      const response = await axios.post(
-        `http://localhost:[port]/api/comment/${postId}`,
-        {
-          commentText: newComment,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
 
-      // Update the comments state with the new comment
-      setComments([...comments, response.data.comment]);
-      // Clear the new comment input
-      setNewComment('');
-    } catch (error) {
-      console.error('Error creating comment:', error.message);
-    }
-  };
-
+  
+useEffect(() => {
+  // Fetch comments when the component mounts
+  fetchComments();
+}, [postId, token]);
   return (
-    <Skeleton />
-  );
-};
+    <div>
+      {loadingInitial ? (
+        <Skeleton avatar paragraph={{ rows: 1 }} active />
+      ) : (
+        <>
+
+<Modal
+  title="Comments"
+  centered
+  open={modal20pen}
+  onOk={handleOk}
+  onCancel={handleCancel}
+>
+  {/* <List
+    bordered
+    dataSource={comments}
+    renderItem={(item) => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src={item.userId.user_image} />}
+          title={item.userId.username}
+          description={item.commentText}
+        />
+      </List.Item>
+    )}
+  /> */}
+</Modal>
+
+    </>
+          )}
+    </div>
+    )
+} 
+
+          {/* <Form onFinish={handleCreateComment}>
+            <Form.Item name="newComment">
+              <Input.TextArea
+                rows={4}
+                placeholder="Write a comment..."
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Add Comment
+              </Button>
+            </Form.Item>
+          </Form> */}
+        {/* </> */}
+      {/* )} */}
+//     </div>
+//   );
+// };
 
 export default Comment;
